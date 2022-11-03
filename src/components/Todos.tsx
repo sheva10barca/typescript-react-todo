af834991f5d764c29334e643a7fdf9087e9f42d6
sheva <vladislavshevchenko10@gmail.com>
2022-11-03 20:28:14 +0200

    start

---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+function Todos() {
+   return (
+      <ul>
+         <li>Learn React</li>
+         <li>Learn TypeScript</li>
+         <li>Vamos</li>
+      </ul>
+   );
+}
+
+export default Todos;

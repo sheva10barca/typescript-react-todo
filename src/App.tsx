c0c5553fcdee9e31ffe099701c8167a9529c2b80
sheva <vladislavshevchenko10@gmail.com>
2022-11-04 21:26:03 +0200

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import Todos from "./components/Todos";
+import { useState } from "react";
+
+import NewTodo from "./components/newTodo/NewTodo";
+import Todos from "./components/todos/Todos";
+import Todo from "./models/todo";
 
 function App() {
+   const [todos, setTodos] = useState<Todo[]>([]);
+
+   const addTodoHandler = (todoText: string) => {
+      const newTodo = new Todo(todoText);
+
+      setTodos((prevTodos) => {
+         return prevTodos.concat(newTodo);
+      });
+   };
+
    return (
       <div>
-         <Todos items={['vamos', 'Barca']} />
+         <NewTodo onAddTodo={addTodoHandler} />
+         <Todos items={todos} />
       </div>
    );
 }
